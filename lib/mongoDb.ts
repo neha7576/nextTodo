3db446fc6d06501925053d8e3f94789ef8a82c55
Neha Sharma <nehasharma7576@gmail.com>
2026-03-11 18:29:37 +0530

    upload data

--- lib/mongoDb.ts
+++ lib/mongoDb.ts
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
 
-const MONGO_DB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error("Please define MONGODB_URI");
+}
 
 export const connectDB = async () => {
-  if (!MONGO_DB_URI) {
-    throw new Error("Please define MONGODB_URI");
-  }
+  if (mongoose.connections[0].readyState) return;
 
-  if (mongoose.connection.readyState >= 1) return;
-
-  return mongoose.connect(MONGO_DB_URI);
+  await mongoose.connect(MONGODB_URI);
 };
